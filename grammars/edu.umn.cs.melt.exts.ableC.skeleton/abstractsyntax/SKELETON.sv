e51f972d477d37a45b6dfa056a2b7e9f852e5df3
Lucas Kramer <krame505@umn.edu>
2023-08-21 18:55:20 -0500

    Fix missing import

--- grammars/edu.umn.cs.melt.exts.ableC.skeleton/abstractsyntax/SKELETON.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.skeleton/abstractsyntax/SKELETON.sv
@@ -1,7 +1,7 @@
 grammar edu:umn:cs:melt:exts:ableC:skeleton:abstractsyntax;
 
 imports edu:umn:cs:melt:ableC:abstractsyntax:host;
-
+imports silver:langutil;
 
 abstract production skeleton
 s::Stmt ::=
